c945b8eaaeeeeb467c541c019283248c700139ba
Tonye O <tonyeo@Tonyes-MacBook-Air.local>
2020-05-27 14:59:41 -0600

    components, styles and pages added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import HighScores from './pages/HighScores';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import Game from './pages/Game';
+import GameOver from './pages/GameOver'
+import { Container } from './styled/container'
+import { Main } from './styled/main';
+import Global from './styled/global'
+
 
 function App() {
   return (
-    <div>
-      <h1>App</h1>
-      
-    </div>
+    <Router>
+      <Global />
+      <Main>
+        <Container>
+          <NavBar />
+          <Switch>
+          
+              <Route path="/highScores" component={HighScores} />
+              <Route path="/game" component={Game} />
+              <Route path="/gameOver" component={GameOver} />
+              <Route path="/" component={Home} />
+          </Switch>
+        </Container>
+      </Main>
+    </Router>
   );
 }
 
