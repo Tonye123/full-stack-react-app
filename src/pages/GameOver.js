85b464e21684be5dc43831bde62a96c01ddc8e60
Tonye O <tonyeo@Tonyes-MacBook-Air.local>
2020-05-30 18:32:08 -0600

    saving high score at the end of each game, setting messages on game over component

--- src/pages/GameOver.js
+++ src/pages/GameOver.js
@@ -1,17 +1,45 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ScoreContext } from '../contexts/ScoreContext'
 import { StyledLink } from '../styled/navBar'
+import { StyledCharacter } from '../styled/game';
 
 
 export default function ({history}) {
     const[score] = useContext(ScoreContext)
+    const[scoreMessage,setScoreMessage] = useState('');
     if(score === -1){
         history.push('/')
 
     }
+
+    useEffect(()=>{
+        const saveHighScore = async () => {
+            try {
+                const options = {
+                    method: 'POST',
+                    body:JSON.stringify({name:"Jamison", score})
+                }
+                const res = await fetch('/.netlify/functions/saveHighScore',options)
+                const data = await res.json();
+                if(data.id){
+                    setScoreMessage('Congrats! you got a High Score!!!!!')
+                }else {
+                    setScoreMessage('OOPs no high score this time, Try again')
+                }
+
+               
+                
+            } catch (error) {
+                console.error(error)
+                
+            }
+        }
+        saveHighScore();
+    }, [])
     return (
         <div>
-            <h1>GameOver: your score:{score}</h1>
+            <StyledCharacter>{score}</StyledCharacter>
+            <p>{scoreMessage}</p>
             <StyledLink to='/'>Go Home</StyledLink>
             <StyledLink to='/game'>PlayAgain?</StyledLink>
         </div>
